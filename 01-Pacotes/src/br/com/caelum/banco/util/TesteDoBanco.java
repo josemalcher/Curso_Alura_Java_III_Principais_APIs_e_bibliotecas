345494a42cf7939b4585aeb1b2723298db3e4dd1
José Malcher Jr <malcher.malch@gmail.com>
2016-09-27 14:20:34 -0300

    Aula 01 - Pacotes

---
+++ 01-Pacotes/src/br/com/caelum/banco/util/TesteDoBanco.java
@@ -0,0 +1,15 @@
+package br.com.caelum.banco.util;
+
+//usando o import
+import br.com.caelum.banco.*;
+/*
+ * Isso faz com que não precisemos nos referenciar utilizando o ::fully qualified name::, podendo utilizar Banco dentro do nosso código em vez de escrever o longo br.com.caelum.banco.Banco.
+ */
+
+public class TesteDoBanco {
+	public static void main(String[] args) {
+		br.com.caelum.banco.Banco meuBanco = new br.com.caelum.banco.Banco();
+		meuBanco.nome = "Banco do Brasil";
+		System.out.println(meuBanco.nome);
+	}
+}
