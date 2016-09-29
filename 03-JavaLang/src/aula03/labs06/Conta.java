1d8e33bb460f85dd872ce4960a03fbcce8245f7e
José Malcher Jr <malcher.malch@gmail.com>
2016-09-29 14:26:39 -0300

    Aula 03 Labs 08 - equals

--- 03-JavaLang/src/aula03/labs06/Conta.java
+++ 03-JavaLang/src/aula03/labs06/Conta.java
@@ -1,7 +1,7 @@
 package aula03.labs06;
 
 public abstract class Conta {
-	
+	private String numeroConta;
 	private double saldo;
 	
 	public double getSaldo() {
@@ -14,6 +14,29 @@ public abstract class Conta {
 	@Override
 	public String toString() {
 		return "Conta [saldo=" + saldo + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((numeroConta == null) ? 0 : numeroConta.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (numeroConta == null) {
+			if (other.numeroConta != null)
+				return false;
+		} else if (!numeroConta.equals(other.numeroConta))
+			return false;
+		return true;
 	}
 	
 	
