720f234e1a73bdaeb8294f7cb87c994fda8098ac
José Malcher Jr <malcher.malch@gmail.com>
2016-09-29 10:59:41 -0300

    Aula 03 Labs 01 - String

---
+++ 03-JavaLang/src/aula03/labs01/TestaString.java
@@ -0,0 +1,12 @@
+package aula03.labs01;
+
+public class TestaString {
+	public static void main(String[] args) {
+		
+		
+		String s = "jf11";
+		s = s.replace("1", "2");
+		System.out.println(s);
+		
+	}
+}
