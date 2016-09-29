468048f03e0ae12c7040eaf67244e426f00b8910
José Malcher Jr <malcher.malch@gmail.com>
2016-09-29 11:03:04 -0300

    Aula 03 Labs 02 - String

--- 03-JavaLang/src/aula03/labs01/TestaString.java
+++ 03-JavaLang/src/aula03/labs01/TestaString.java
@@ -2,11 +2,27 @@ package aula03.labs01;
 
 public class TestaString {
 	public static void main(String[] args) {
-		
-		
+
 		String s = "jf11";
 		s = s.replace("1", "2");
 		System.out.println(s);
-		
+
+		int qntCarac = s.length();
+		System.out.println("Quantidade de caracteres= " + qntCarac);
+		/*
+		 * contains: devolve true se a String contem a sequência de caracteres
+		 * passada;
+		 * 
+		 * trim: devolve uma nova String sem caracteres brancos do início e do
+		 * fim;
+		 * 
+		 * isEmpty: devolve true se a String está vazia. Surgiu no Java 6;
+		 * 
+		 * length: devolve a quantidade de caracteres da String.
+		 * 
+		 * http://docs.oracle.com/javase/7/docs/api/java/lang/String.html
+		 * 
+		 */
+
 	}
 }
