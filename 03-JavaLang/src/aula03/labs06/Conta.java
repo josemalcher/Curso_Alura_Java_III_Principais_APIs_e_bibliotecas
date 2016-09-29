6b92cfe1bfe970f80142010bd4e5babe313e50de
José Malcher Jr <malcher.malch@gmail.com>
2016-09-29 14:18:49 -0300

    Aula 03 Labs 07 - toString

--- 03-JavaLang/src/aula03/labs06/Conta.java
+++ 03-JavaLang/src/aula03/labs06/Conta.java
@@ -1,5 +1,21 @@
 package aula03.labs06;
 
-abstract class Conta {
+public abstract class Conta {
+	
+	private double saldo;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	private void set() {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public String toString() {
+		return "Conta [saldo=" + saldo + "]";
+	}
+	
+	
 
 }
