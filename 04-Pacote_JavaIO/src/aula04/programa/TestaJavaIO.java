16b67f95c2b2ba6485eae153b5dbffc0267acc2a
José Malcher Jr <malcher.malch@gmail.com>
2016-09-30 16:45:59 -0300

    Aula 04 - Pacote Java.io/Leitura e escrita - Parte 3 lendo e escrevendo pelo console - 8:00m/09:31m

--- 04-Pacote_JavaIO/src/aula04/programa/TestaJavaIO.java
+++ 04-Pacote_JavaIO/src/aula04/programa/TestaJavaIO.java
@@ -14,10 +14,12 @@ public class TestaJavaIO {
 	public static void main(String[] args) throws IOException {
 		
 		InputStream is = new FileInputStream("leitura.txt");
+		//InputStream is = System.in;
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
 		
 		OutputStream os = new FileOutputStream("saida.txt");
+		//OutputStream os = System.out;
 		OutputStreamWriter osw = new OutputStreamWriter(os);
 		BufferedWriter bw = new BufferedWriter(osw);
 		
