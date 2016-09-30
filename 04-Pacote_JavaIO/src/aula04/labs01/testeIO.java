1ebdb718933907f63c3926638112d14e73d4fafc
José Malcher Jr <malcher.malch@gmail.com>
2016-09-30 17:38:58 -0300

    Aula 04 Labs 01 - Exerc com imputStream e BufferedReader usando o console para ler e escrever

---
+++ 04-Pacote_JavaIO/src/aula04/labs01/testeIO.java
@@ -0,0 +1,31 @@
+package aula04.labs01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/*
+ * Exercício 1
+Crie um projeto novo chamado testeIO. E, nele, crie um programa (simplesmente uma classe com um main) que leia da entrada padrão. Para isso, você vai precisar de um BufferedReader que leia do System.in da mesma forma como fizemos.
+Não digite esses nomes de classes complicados! Lembre-se de fazer como o instrutor e escrever primeiro a parte depois do igual. Então, use o ctrl + 1 para que o Eclipse crie a variável para você! Assim mesmo, enquanto você escreve a parte da direita, abuse do ctrl + espaço porque além de te ajudar com o nome, ele colocará o import no devido lugar. 
+Cuidado: existe mais de uma classe chamada InputStream: queremos a do pacote java.io.
+
+ */
+public class testeIO {
+	public static void main(String[] args) throws IOException {
+
+		InputStream is = System.in;
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+
+		System.out.println("Digite a mensagem aqui!");
+		String linha = br.readLine();
+
+		while (linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+
+	}
+}
--- 04-Pacote_JavaIO/src/aula04/programa/TestaJavaIO.java
+++ 04-Pacote_JavaIO/src/aula04/programa/TestaJavaIO.java
@@ -1,18 +1,20 @@
 package aula04.programa;
 
-import java.io.BufferedReader;
+/*import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.OutputStreamWriter;*/
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class TestaJavaIO {
 	public static void main(String[] args) throws IOException {
-		
+		/*
 		InputStream is = new FileInputStream("leitura.txt");
 		//InputStream is = System.in;
 		InputStreamReader isr = new InputStreamReader(is);
@@ -35,7 +37,16 @@ public class TestaJavaIO {
 		br.close();
 		bw.close();
 		
-
+*/
+		Scanner sc = new Scanner(new FileInputStream("leitura.txt"));
+		PrintStream ps = new PrintStream("saida.txt");
+		
+		while(sc.hasNextLine()){
+			String linha = sc.nextLine();
+			ps.println(linha);
+		}
+		ps.close();
+		sc.close();
 		
 	}
 }
