76c53888a40b4d59554e9070d530f59e5f7c0b45
José Malcher Jr <malcher.malch@gmail.com>
2016-09-30 16:29:10 -0300

    Aula 04 - Pacote Java.io/Leitura e escrita - Parte 1 Leitura - 4:30m

---
+++ 04-Pacote_JavaIO/src/aula04/programa/TestaJavaIO.java
@@ -0,0 +1,26 @@
+package aula04.programa;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class TestaJavaIO {
+	public static void main(String[] args) throws IOException {
+		
+		InputStream is = new FileInputStream("leitura.txt");
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		
+		String linha = br.readLine();
+		
+		while(linha != null){
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+		
+		br.close();
+		
+	}
+}
