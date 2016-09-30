59612138a9417b265dfddde23982e3ecb69b45ca
José Malcher Jr <malcher.malch@gmail.com>
2016-09-30 16:38:29 -0300

    Aula 04 - Pacote Java.io/Leitura e escrita - Parte 2 saída - 6:30m/09:31m

--- 04-Pacote_JavaIO/src/aula04/programa/TestaJavaIO.java
+++ 04-Pacote_JavaIO/src/aula04/programa/TestaJavaIO.java
@@ -1,10 +1,14 @@
 package aula04.programa;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class TestaJavaIO {
 	public static void main(String[] args) throws IOException {
@@ -13,14 +17,23 @@ public class TestaJavaIO {
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
 		
+		OutputStream os = new FileOutputStream("saida.txt");
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
 		String linha = br.readLine();
 		
 		while(linha != null){
-			System.out.println(linha);
+			//System.out.println(linha);
+			bw.append(linha);
+			bw.newLine();
 			linha = br.readLine();
 		}
 		
 		br.close();
+		bw.close();
+		
+
 		
 	}
 }
